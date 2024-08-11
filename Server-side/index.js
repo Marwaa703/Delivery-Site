9f0f550a27bee75df7917adb87e759380b6d1fdb
marwa <meroblue25@gmail.com>
2024-08-11 19:14:01 +0300

    modify

--- Server-side/index.js
+++ Server-side/index.js
@@ -43,6 +43,7 @@ const loadData = () => {
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 const ownersFilePath = path.join(__dirname, "owners.json");
 const restaurantsFilePath = path.join(__dirname, "restaurants.json");
 const ordersFilePath = path.join(__dirname, "orders.json");
@@ -113,6 +114,11 @@ const authenticateToken = (req, res, next) => {
     next();
   });
 };
+
+//Fallback Route
+app.get("*", (req, res) => {
+  res.status(404).send("404 - Not Found");
+});
 
 // Route to fetch user info
 app.get("/user-info", authenticateToken, (req, res) => {
